2d5de9faaadd0ad167d791f48d237bcd662dfd38
elenarivero <145675391+elenarivero@users.noreply.github.com>
2024-06-05 07:59:25 +0200

    Ejercicios Tema 2

--- src/ejerciciost2bucles/Ejercicio1.java
+++ src/ejerciciost2bucles/Ejercicio1.java
@@ -5,51 +5,61 @@ import java.util.Scanner;
 
 public class Ejercicio1 {
 
-	public static void main(String[] args) {
-		int horas=-1, min=-1, segundos=-1;
-		int incremento;
+	
+	public static int leeNumero(Scanner scan) {
+		int num = -1;
 		
+		try {
+			
+			num = scan.nextInt();
+		} catch (InputMismatchException e) {
+			System.out.println("El tipo no es correcto");
+			scan.nextLine();
+		} 
+		return num;
+	}
+
+	public static void main(String[] args) {
+		int horas = -1, min = -1, segundos = -1;
+		int incremento;	
+
 		Scanner sc = new Scanner(System.in);
 		
 		do {
-			try {
-				System.out.println("Introduzca las horas:");
-				horas = sc.nextInt();
-			} catch(InputMismatchException e) {
-				System.out.println("El dato introducido no es del tipo correcto");
-				sc.nextLine();
-			}
-			
-		}while(horas<0 || horas>23);
+			System.out.println("Introduzca las horas:");
+			horas = leeNumero(sc);
+		} while (horas < 0 || horas > 23);
+
+		System.out.println("horas: " + horas);
 		
 		do {
 			try {
 				System.out.println("Introduzca los minutos:");
 				min = sc.nextInt();
-				if(min < 0 || min > 59) {
+				if (min < 0 || min > 59) {
 					System.out.println("Dato erróneo. Vuelva a introducirlo");
 				}
-			} catch(InputMismatchException e) {
+			} catch (InputMismatchException e) {
 				System.out.println("El dato introducido no es del tipo correcto");
 				sc.nextLine();
 			}
-			
-		}while(min<0 || min>59);
-		
+
+		} while (min < 0 || min > 59);
+
 		do {
 			try {
 				System.out.println("Introduzca los segundos:");
 				segundos = sc.nextInt();
-				
-			} catch(InputMismatchException e) {
+
+			} catch (InputMismatchException e) {
 				System.out.println("El dato introducido no es del tipo correcto");
 				sc.nextLine();
 			}
-			
-		}while(segundos<0 || segundos>59);
-		
+
+		} while (segundos < 0 || segundos > 59);
+
 		System.out.println("Horas: " + horas);
-		
+
 		sc.close();
 	} // fin main
 
--- src/ejerciciost2bucles/Ejercicio10.java
+++ src/ejerciciost2bucles/Ejercicio10.java
@@ -1,26 +1,34 @@
 package ejerciciost2bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio10 {
 
 	public static void main(String[] args) {
 		// Variable para guardar el número
-		int num;
+		int num = -1;
 		int auxiliar;
 		int numReves = 0;
 		int ultimaCifra;
-		
+
 		// Creamos el Scanner para leer de teclado
 		Scanner sc = new Scanner(System.in);
-		
-		// Pedimos un número por teclado
-		System.out.println("Introduzca un número positivo:");
-		num = sc.nextInt();
-		
+
+		do {
+			try {
+				// Pedimos un número por teclado
+				System.out.println("Introduzca un número positivo:");
+				num = sc.nextInt();
+			} catch (InputMismatchException e) {
+				System.out.println("El tipo introducido es incorrecto");
+				sc.nextLine();
+			}
+		} while (num <= 0);
+
 		auxiliar = num;
-		while(auxiliar>0) {
-			ultimaCifra = auxiliar%10;
+		while (auxiliar > 0) {
+			ultimaCifra = auxiliar % 10;
 			numReves = numReves * 10 + ultimaCifra;
 			auxiliar /= 10;
 		}
@@ -30,8 +38,7 @@ public class Ejercicio10 {
 		} else {
 			System.out.println("No es capicúa");
 		}
-		
+
 		sc.close();
 	}
-
 }
